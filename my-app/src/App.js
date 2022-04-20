e769ad72a7539854f020b2f9c61ca6542d31a015
ian <ianmoraes1@gmail.com>
2022-04-20 19:42:45 -0300

    a

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,20 @@ import React, {useState} from 'react';
 import './App.css';
 import Button from "@mui/material/Button";
 import { motion } from "framer-motion";
+import { useRef } from "react";
 function App() {
   const [clicks, setClicks] = useState(0)
  
+  const constraintsRef = useRef(null);
 
+  
   return (
-    <div>
+    
+    <div className="flex">
       <p>clicou {clicks} vezes</p>
-      <motion.div 
-      whileHover={{ borderRadius: "100%"}}
-      >
-        <Button  variant="outlined" onClick={()=>setClicks(clicks+1)}>Clique aqui +</Button>
+      <motion.div className="container" ref={constraintsRef}>
+        <motion.div className="item" drag dragConstraints={constraintsRef} ><Button  variant="outlined" onClick={()=>setClicks(clicks+2)}>Clique aqui +</Button></motion.div>
       </motion.div>
-      <motion.div 
-      whileHover={{ borderRadius: "100%"}}
-      >
-      <Button variant="outlined" onClick={()=>setClicks(clicks-
-        1)}>Clique aqui -</Button>
-    </motion.div>
 
 
     </div>
